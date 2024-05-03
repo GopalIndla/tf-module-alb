0092229d6c15d25d897fdd79c54eb24cfd8170f4
CodingManoj <CodingManoj@gmail.com>
2024-05-02 21:34:55 -0400

    Auto Push

--- alb.tf
+++ alb.tf
@@ -1,14 +1,18 @@
 # Provisions ALB
-# resource "aws_lb" "alb" {
-#   name               = "test-lb-tf"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.lb_sg.id]
-#   subnets            = [for subnet in aws_subnet.public : subnet.id]
+resource "aws_lb" "alb" {
+  name               = var.ALB_NAME
+  internal           = var.INTERNAL
+  load_balancer_type = "application"
+  security_groups    = var.INTERNAL ? aws_security_group.allow_private.*.id  : aws_security_group.allow_public.*.id 
+  subnets            = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS : data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
 
-#   enable_deletion_protection = false 
+  enable_deletion_protection = false 
 
-#   tags = {
-#     Environment = var.ALB_NAME
-#   }
-# }
+  tags = {
+    Environment = var.ALB_NAME
+  }
+}
+
+
+# Public-Module  --->  Public ALB  + Public SG  + Public Subnets 
+# Provate-Module --->  Private ALB + Private SG + Private Subnets 
